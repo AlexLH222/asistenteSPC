7d38010e9c6a0c6a7fd7d66447ea04e2e740da7c
Alex Luque <alex.xxiv.04@gmail.com>
2025-06-18 23:30:12 -0500

    Cambio en el main , puerto 8000

--- main.py
+++ main.py
@@ -220,3 +220,8 @@ async def chat_endpoint(chat: ChatRequest):
 @app.get("/")
 async def root():
     return {"message": "Backend funcionando correctamente"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
